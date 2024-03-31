4846b31d140896fe1ec06e1764b5d0b4c31cd18a
kjun <kjun_all@naver.com>
2024-03-31 23:56:59 +0900

    Feat: FeedHomeScreen 구현

---
+++ front/src/screens/feed/FeedHomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function FeedHomeScreen({}: FeedhomeScreenProps) {
+  return (
+    <View>
+      <Text>피드</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default FeedHomeScreen;
