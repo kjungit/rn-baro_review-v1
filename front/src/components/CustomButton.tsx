808213f5090b29601576ff456ed9e7e7892e5e9f
kjun <kjun_all@naver.com>
2024-04-01 00:57:53 +0900

    Feat: CustomButton 컴포넌트 구현

---
+++ front/src/components/CustomButton.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  PressableProps,
+  Dimensions,
+} from 'react-native';
+import {colors} from '../constants';
+
+interface CustomButtonProps extends PressableProps {
+  label: string;
+  variant?: 'filled' | 'outlined';
+  size?: 'lg' | 'md';
+  inValid?: boolean;
+}
+const deviceHeight = Dimensions.get('screen').height;
+function CustomButton({
+  label,
+  variant = 'filled',
+  size = 'lg',
+  inValid = false,
+  ...props
+}: CustomButtonProps) {
+  return (
+    <Pressable
+      disabled={inValid}
+      style={({pressed}) => [
+        styles.container,
+        styles[variant],
+        styles[size],
+        pressed ? styles[`${variant}Pressed`] : styles[variant],
+        inValid && styles.inValid,
+      ]}
+      {...props}>
+      <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 3,
+    justifyContent: 'center',
+  },
+  filled: {
+    backgroundColor: colors.PINK_700,
+  },
+  outlined: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+  },
+  inValid: {
+    opacity: 0.5,
+  },
+  lg: {
+    width: '100%',
+    paddingVertical: deviceHeight > 740 ? 16 : 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  md: {
+    width: '50%',
+    paddingVertical: deviceHeight > 740 ? 12 : 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  filledText: {
+    color: colors.WHITE,
+  },
+  outlinedText: {
+    color: colors.PINK_700,
+  },
+  filledPressed: {
+    backgroundColor: colors.PINK_500,
+  },
+  outlinedPressed: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+    opacity: 0.5,
+  },
+});
+
+export default CustomButton;
