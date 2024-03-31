99148a28949ac272b9dab725800ab247fee59972
kjun <kjun_all@naver.com>
2024-04-01 00:58:05 +0900

    Feat: 컴포넌트 연결 및 경로 수정

--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,30 +1,31 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
+import CustomButton from '../../components/CustomButton';
 import {authNavigations} from '../../constants';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
   typeof authNavigations.AUTH_HOME
 >;
+
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
     <SafeAreaView>
       <View>
-        <Button
-          title="로그인 화면으로 이동"
+        <CustomButton
+          label="로그인 하기"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
-        <Button
-          title="회원가입 화면으로 이동"
+        <CustomButton
+          label="회원가입 하기"
+          variant="outlined"
           onPress={() => navigation.navigate(authNavigations.SIGNUP)}
         />
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({});
 
 export default AuthHomeScreen;
