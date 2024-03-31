7c5e6863b168064571b2b43256ce893afb33735c
kjun <kjun_all@naver.com>
2024-03-31 23:56:44 +0900

    Feat: CalendarHomeScreen 구현

---
+++ front/src/screens/calendar/CalendarHomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface CalendarHomeScreenProps {}
+
+function CalendarHomeScreen({}: CalendarHomeScreenProps) {
+  return (
+    <View>
+      <Text>Calendar</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default CalendarHomeScreen;
