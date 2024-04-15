acac9fdfa733b5f101dd1cab2f5703885302ed14
kjun <kjun_all@naver.com>
2024-04-15 23:01:21 +0900

    style: color 상수 추가

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,5 +1,11 @@
 export const colors = {
   WHITE: '#fff',
+  BLACK: '#000',
+  Red_300: '#FFB4B4',
+  Red_500: '#be3c3c',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8E8E8E',
+  GRAY_700: '#575757',
 };
