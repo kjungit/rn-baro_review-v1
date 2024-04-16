0aa98917af4a529abc9098d1c4e087912914cace
kjun <kjun_all@naver.com>
2024-04-16 23:45:42 +0900

    Feat: LoginScreen input 자동 넘김 및 기능 추가 - ref 연결하여 다음 input으로 넘길 수있도록 수정 - returnKeyType, blurOnSubmit, onSubmitEditing 기능 추가

--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
 import InputField from '../../components/InputField';
 import CustomButton from '../../components/CustomButton';
 import useForm from '../../hooks/useForm';
@@ -8,6 +8,8 @@ import {validateLogin} from '../../utils';
 interface LoginScreenProps {}
 
 function LoginScreen({}: LoginScreenProps) {
+  const passwordRef = useRef<TextInput | null>(null);
+
   const login = useForm({
     initialValue: {
       email: '',
@@ -24,18 +26,25 @@ function LoginScreen({}: LoginScreenProps) {
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="이메일"
           error={login.errors.email}
           touched={login.touched.email}
           inputMode="email"
           {...login.getTextInputProps('email')}
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           error={login.errors.password}
           touched={login.touched.password}
           {...login.getTextInputProps('password')}
           secureTextEntry
+          returnKeyType="join"
+          onSubmitEditing={handleSubmit}
         />
       </View>
       <CustomButton
