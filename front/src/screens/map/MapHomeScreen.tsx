f8c5d7220986804ba29d80940be288bcf2b5271c
kjun <kjun_all@naver.com>
2024-04-20 20:10:56 +0900

    Feat: 로그아웃 로직 연결

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,10 +1,13 @@
+import useAuth from '@/hooks/queries/useAuth';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const MapHomeScreen = () => {
+  const {logoutMutation} = useAuth();
   return (
     <View>
       <Text>멥 스크린</Text>
+      <Button title="로그아웃" onPress={() => logoutMutation.mutate(null)} />
     </View>
   );
 };
