e4821628a7e25d5c0545d4d3d1f9be965877c489
kjun <kjun_all@naver.com>
2024-04-20 19:43:59 +0900

    init: eslint 설정 변경

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react-hooks'],
+  rules: {
+    indent: ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-closing-bracket-location': ['error', 'after-prop'],
+    'react/jsx-curly-spacing': ['error', 'never'],
+    'react/jsx-uses-vars': ['error'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
